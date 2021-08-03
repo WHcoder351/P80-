24ea291dc3ebb189ab7c256c8c890e07d26f5785
WHcoder351 <76533189+WHcoder351@users.noreply.github.com>
2021-08-03 13:54:30 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ names = [];
 function submit(){
 var  stuname = [];
     for (var j = 1; j<=4; j++ ){
-        var putname = document.getElementById("name_of_the_student_"+j).value;
-        console.log(putname);
-        names.push(putname);
+        stuname.push(document.getElementById("name_of_the_student_"+j).value);
+        document.getElementById("DisWCom").innerHTML = stuname.join(". ");
+        document.getElementById("DisWTCom").innerHTML = stuname.join(". ");
+
     }
     console.log(names);
     var lon= names.length;
@@ -21,28 +22,7 @@ var  stuname = [];
 
 
 }
-function submit2(){
-    var  stuname2 = [];
-        for (var j = 0; j<=8; j++ ){
-            var putname = document.getElementById("name_of_the_student_"+l).value;
-            console.log(putname);
-            names.push(putname);
-        }
-        console.log(names);
-        var lon= names.length;
-        console.log(lon);
-        for (var k=0; k<lon; k++){
-            stuname2.push(names[k]);
-            console.log (stuname2);
-        }
-        var removecom = stuname_sorting.join("");
-        console.log(removecom);
-        console.log (stuname2);
-        document.getElementById("DisWTCom").innerHTML=stuname2;
-     
-    
-    
-    }
+
     var removecom = stuname_sorting.join("");
     console.log(removecom);
 
