001931ba597f2125ded62ff701208f0cf98e3e32
WHcoder351 <76533189+WHcoder351@users.noreply.github.com>
2021-08-04 14:36:13 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ var  stuname = [];
     for (var j = 1; j<=4; j++ ){
         stuname.push(document.getElementById("name_of_the_student_"+j).value);
         document.getElementById("DisWCom").innerHTML = stuname.join(". ");
-        document.getElementById("DisWTCom").innerHTML = stuname.join(". ");
-
     }
     console.log(names);
     var lon= names.length;
@@ -22,6 +20,30 @@ var  stuname = [];
 
 
 }
+
+
+function submit2(){
+    var  stuname = [];
+        for (var j = 4; j<=8; j++ ){
+            stuname.push(document.getElementById("name_of_the_student_"+j).value);
+            document.getElementById("DisWTCom").innerHTML = stuname.join(". ");
+    
+        }
+        console.log(names);
+        var lon= names.length;
+        console.log(lon);
+        for (var k=0; k<lon; k++){
+            stuname.push(names[k]);
+            console.log (stuname);
+        }
+        var removecom = stuname_sorting.join("");
+        console.log(removecom);
+        console.log (stuname);
+        document.getElementById("DisWCom").innerHTML=stuname;
+     
+    
+    
+    }
 
     var removecom = stuname_sorting.join("");
     console.log(removecom);
