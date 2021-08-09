ec93a62f56fb9831ae661a6ef9ac68e05ceccfba
WHcoder351 <76533189+WHcoder351@users.noreply.github.com>
2021-08-09 11:57:41 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ names = [];
 function submit(){
 var  stuname = [];
     for (var j = 1; j<=4; j++ ){
-        stuname.push(document.getElementById("name_of_the_student_"+j).value);
-        document.getElementById("DisWCom").innerHTML = stuname.join(". ");
+        var putname = document.getElementById("name_of_the_student_"+j).value;
+        console.log(putname);
+        names.push(putname);
     }
     console.log(names);
     var lon= names.length;
@@ -20,31 +21,28 @@ var  stuname = [];
 
 
 }
-
-
 function submit2(){
-    var  stuname = [];
-        for (var j = 4; j<=8; j++ ){
-            stuname.push(document.getElementById("name_of_the_student_"+j).value);
-            document.getElementById("DisWTCom").innerHTML = stuname.join(". ");
-    
+    var  stuname2 = [];
+        for (var j = 0; j<=8; j++ ){
+            var putname = document.getElementById("name_of_the_student_"+l).value;
+            console.log(putname);
+            names.push(putname);
         }
         console.log(names);
         var lon= names.length;
         console.log(lon);
         for (var k=0; k<lon; k++){
-            stuname.push(names[k]);
-            console.log (stuname);
+            stuname2.push(names[k]);
+            console.log (stuname2);
         }
         var removecom = stuname_sorting.join("");
         console.log(removecom);
-        console.log (stuname);
-        document.getElementById("DisWCom").innerHTML=stuname;
+        console.log (stuname2);
+        document.getElementById("DisWTCom").innerHTML=stuname2;
      
     
     
     }
-
     var removecom = stuname_sorting.join("");
     console.log(removecom);
 
